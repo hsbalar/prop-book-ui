35711a29adba5d351f68376a302d946a1fa256fa
Hitesh Balar <hsbalar@gmail.com>
2021-08-07 13:07:40 +0530

    Github actions for parse env

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -5,7 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('envars');
   console.log(`Hello ${nameToGreet}!`);
-  // core.setOutput('time', time);
+  core.setOutput('port', 1000);
   // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
   core.setFailed(error.message);
