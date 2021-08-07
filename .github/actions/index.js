0b8504f32347ec68966fa76516826f6a2c5fe7d4
Hitesh Balar <hsbalar@gmail.com>
2021-08-07 14:15:23 +0530

    Test dynamic env substitute

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -3,10 +3,10 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('envars');
-  console.log(`Hello ${nameToGreet}!`);
-  console.log(JSON.parse(nameToGreet));
-  core.setOutput('port', 1000);
+  const envObj = core.getInput('envars');
+  Object.keys(envObj).forEach((key) => {
+    core.setOutput(key, envObj[key]);
+  });
   // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
   core.setFailed(error.message);
