6c95323c40ee34ee9034d636da0f079696225fff
Hitesh Balar <hsbalar@gmail.com>
2021-08-08 00:13:34 +0530

    Parse JSON

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,14 +4,14 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const envObj = core.getInput('envars');
-  // const envs = JSON.parse(envObj);
+  const envs = JSON.parse(envObj);
   // console.log(envs);
   // console.log(envs['url']);
   // core.setOutput('url', envs.url);
   // Object.keys(envs).forEach(function (key) {
   //   console.log(key, envs[key]);
   // });
-  core.setOutput('port', envObj);
+  core.setOutput('port', envs);
 
   // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
