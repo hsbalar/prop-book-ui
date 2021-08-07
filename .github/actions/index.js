cb39568871ae2b701b3d82c925f39397b1eebfad
Hitesh Balar <hsbalar@gmail.com>
2021-08-07 13:46:35 +0530

    Fix indentation

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -5,6 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('envars');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(JSON.parse(nameToGreet));
   core.setOutput('port', 1000);
   // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
