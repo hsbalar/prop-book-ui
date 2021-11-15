ea4b03fa57fdf0648434c6a8c8d864117b1b058f
Hitesh <hsbalar@gmail.com>
2021-11-15 16:45:52 +0530

    read spec from workflow to pass docker build

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,17 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const envObj = core.getInput('envars');
   const envs = JSON.parse(envObj || '{}');
+
+  fs.readFile('/spec', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    core.setOutput('build_vars', data);
+  });
   // console.log(envs);
   // console.log(envs['url']);
   // core.setOutput('url', envs.url);
   // Object.keys(envs).forEach(function (key) {
   //   console.log(key, envs[key]);
   // });
-  core.setOutput('port', envs);
+  
 
   // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
